6200601733c8169475a7f883d93e5dcfe1fda4c6
asutosh-das <ashutoshdas788@gmail.com>
2026-02-14 21:29:28 +0530

    hero component ready for home page

--- src/components/student/SearchBar.jsx
+++ src/components/student/SearchBar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const SearchBar = () => {
   return (
     <div>
-      <h1>SearchBar</h1>
+      <form action="">
+        <img src={assets.search_icon} alt="search_icon"  className='md:w'/>
+      </form>
     </div>
   )
 }
