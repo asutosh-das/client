99e86558aa682532255285fd2c49beabe8eb554d
asutosh-das <ashutoshdas788@gmail.com>
2026-02-14 21:57:10 +0530

    Companies added to home

--- src/components/student/SearchBar.jsx
+++ src/components/student/SearchBar.jsx
@@ -2,23 +2,28 @@ import React, { useState } from "react";
 import { assets } from "../../assets/assets";
 import { useNavigate } from "react-router-dom";
 
-const SearchBar = () => {
-
-
+const SearchBar = ({data}) => {
   const navigate = useNavigate();
-  const [input, setInput] = useState(data ? data : '')
+  const [input, setInput] = useState(data ? data : "");
 
-  const on
-
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+    navigate("/course-list/" + input);
+  };
 
   return (
-    <form className="max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded">
+    <form
+      onSubmit={onSearchHandler}
+      className="max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded"
+    >
       <img
         src={assets.search_icon}
         alt="search_icon"
         className="md:w-auto w-10 px-3"
       />
       <input
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
         type="text"
         placeholder="Search for courses"
         className="w-full h-full outline-none text-gray-500/80"
