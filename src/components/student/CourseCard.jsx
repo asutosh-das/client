bd616709f1e0c00f92aaeccdfdb3f6909d295702
asutosh-das <ashutoshdas788@gmail.com>
2026-02-15 20:38:32 +0530

    replacing static  feedback  with dynamic feedback

--- src/components/student/CourseCard.jsx
+++ src/components/student/CourseCard.jsx
@@ -4,23 +4,35 @@ import { assets } from "../../assets/assets";
 import { AppContext } from "../../context/AppContext";
 
 const CourseCard = ({ course }) => {
-  const { currency } = useContext(AppContext);
+  const { currency, calculateRating } = useContext(AppContext);
 
   return (
-    <Link to={"/course/" + course._id} onClick={() => scrollTo(0, 0)}
-    className='border border-gray-500/30 pb-6 overflow-hidden rounded-lg'>
+    <Link
+      to={"/course/" + course._id}
+      onClick={() => scrollTo(0, 0)}
+      className="border border-gray-500/30 pb-6 overflow-hidden rounded-lg"
+    >
       <img className="w-full" src={course.courseThumbnail} alt="" />
       <div className="p-3 text-left">
         <h3 className="text-base font-semibold">{course.courseTitle}</h3>
         <p className="text-gray-500">{course.educator.name}</p>
         <div className="flex items-center space-x-2">
-          <p>4.5</p>
+          <p>{calculateRating(course)}</p>
           <div className="flex">
             {[...Array(5)].map((_, i) => (
-              <img key={i} src={assets.star} alt="" className="w-3.5 h-3.5"/>
+              <img
+                key={i}
+                src={
+                  i < Math.floor(calculateRating(course))
+                    ? assets.star
+                    : assets.star_blank
+                }
+                alt=""
+                className="w-3.5 h-3.5"
+              />
             ))}
           </div>
-          <p className="text-gray-500">22</p>
+          <p className="text-gray-500">{course.courseRatings.length}</p>
         </div>
         <p className="text-base font-semibold text-gray-800">
           {currency}
