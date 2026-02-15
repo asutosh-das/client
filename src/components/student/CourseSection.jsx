9ada35e2cf83f0ce8f1fb8d4402d3420bb4841fd
asutosh-das <ashutoshdas788@gmail.com>
2026-02-15 20:18:52 +0530

    Course card added to page, with responsive flow

--- src/components/student/CourseSection.jsx
+++ src/components/student/CourseSection.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
+import CourseCard from "./CourseCard";
 
 const CourseSection = () => {
+  const { allCourses } = useContext(AppContext);
+
   return (
     <div className="py-16 md:px-40 px-8 text-center">
       <h2 className="text-3xl font-medium text-gray-800">
@@ -9,9 +13,15 @@ const CourseSection = () => {
       </h2>
       <p className="text-sm md:text-base text-gray-500 mt-3">
         Discover our top-rated courses across various categories. From coding
-        and design to business and wellness, our courses are crafted to deliver
+        and design to <br /> business and wellness, our courses are crafted to deliver
         results.
       </p>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-4 md:my-16 my-10 gap-4">
+        {allCourses.slice(0, 4).map((course, index) => (
+          <CourseCard key={index} course={course} />
+        ))}
+      </div>
 
       <Link
         to={"/course-list"}
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,16 +1,28 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+  const currency = import.meta.env.VITE_CURRENCY;
 
-    const value = {
+  const [allCourses, setAllCourses] = useState([]);
 
-    }
+  //Fetch ALl courses
+  const fetchAllCourses = async () => {
+    setAllCourses(dummyCourses);
+  };
 
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
+
+  const value = {
+    currency,
+    allCourses,
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
